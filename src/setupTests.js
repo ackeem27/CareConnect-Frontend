a337d90ed8570642638aafab64e52724f090656b
Picket Zangewa <bsc-com-21-22@unima.ac.mw>
2026-05-20 06:59:53 +0200

    feat: add authentication styles and jest configuration with intersection observer mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock IntersectionObserver for Jest
+class IntersectionObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+window.IntersectionObserver = IntersectionObserver;
